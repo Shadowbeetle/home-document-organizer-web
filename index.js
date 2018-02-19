3842c4d32c9d93d53915e88cbc30df1346765f7c
Shadowbeetle <tamas.kadlecsik@gmail.com>
2018-02-19 19:01:01 +0100

    rename google to db

--- index.js
+++ index.js
@@ -1,44 +1,44 @@
-const google = require('./model/google')
+const db = require('./model/db')
 
-google.api.authenticate()
+db.authenticate()
     .then(() => {
-        google.api.doc.getInfo(function(err, info) {
-            console.log('Loaded doc: '+info.title+' by '+info.author.email)
+        db.doc.getInfo(function (err, info) {
+            console.log('Loaded doc: ' + info.title + ' by ' + info.author.email)
             sheet = info.worksheets[0]
-            console.log('sheet 1: '+sheet.title+' '+sheet.rowCount+'x'+sheet.colCount)
+            console.log('sheet 1: ' + sheet.title + ' ' + sheet.rowCount + 'x' + sheet.colCount)
         })
     })
-    .then(google.api.getLatestInvoiceId)
+    .then(db.getLatestInvoiceId)
     .then(v => console.log('latest invoice id', v))
 
-    .then(google.api.getLatestProductId)
+    .then(db.getLatestProductId)
     .then(v => console.log('latest product id', v))
-    
-    .then(() => google.api.getProductById(3))
+
+    .then(() => db.getProductById(3))
     .then(v => console.log('product no 3 is', v.class))
-    
-    .then(() => google.api.getInvoiceById(3))
+
+    .then(() => db.getInvoiceById(3))
     .then(v => console.log('invoice no 3 is for', v.map(i => i.class)))
-    
-    .then(() => google.api.gatherBrands())
-    .then(console.log.bind(console))
-    
-    .then(() => google.api.gatherClasses())
+
+    .then(() => db.gatherBrands())
     .then(console.log.bind(console))
 
-    .then(() => google.api.gatherMaterials())
+    .then(() => db.gatherClasses())
     .then(console.log.bind(console))
 
-    .then(() => google.api.gatherPlacesOfInvoices())
+    .then(() => db.gatherMaterials())
     .then(console.log.bind(console))
 
-    .then(() => google.api.gatherPlacesOfPurchases())
+    .then(() => db.gatherPlacesOfInvoices())
     .then(console.log.bind(console))
 
-    .then(() => google.api.gatherShops())
+    .then(() => db.gatherPlacesOfPurchases())
     .then(console.log.bind(console))
 
-    .then(() => google.api.gatherTypes())
+    .then(() => db.gatherShops())
+    .then(console.log.bind(console))
+
+    .then(() => db.gatherTypes())
     .then(console.log.bind(console))
 
     .then(() => {
--- model/index.js
+++ model/index.js
@@ -1,3 +1,3 @@
 module.exports = {
-    google: require('./google')
+    db: require('./db')
 }
