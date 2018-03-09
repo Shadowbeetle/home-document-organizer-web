d955e33d78204d7693f00ec064004f2489fc9909
Shadowbeetle <tamas.kadlecsik@gmail.com>
2018-03-09 17:24:17 +0100

    remove unnecessary tools

--- model/db/knex/knex.js
+++ model/db/knex/knex.js
@@ -2,31 +2,18 @@
 
 const _ = require('lodash')
 const createKnex = require('knex')
-const logger = require('winston')
-const promiseTimeout = require('@risingstack/service-utils').functions
-  .promiseTimeout
 const config = require('../../../config/postgres')
+
+const logger = console
 
 // TLS provided
 if (_.get(config, 'postgres.driver.connection.ssl.ca')) {
-  logger.info('Postgres TLS is enabled')
+  logger.log('Postgres TLS is enabled')
 } else {
   delete config.postgres.driver.connection.ssl
 }
 
 const knex = createKnex(config.postgres.driver)
-
-function healthcheck () {
-  const check = knex.raw('SELECT 1')
-
-  return promiseTimeout(check, config.health.timeout).catch(function SQLError (
-    err
-  ) {
-    logger.error('SQL healthcheck failed', err)
-
-    throw err
-  })
-}
 
 function getPoolStats () {
   const pool = knex.client.pool
@@ -43,11 +30,10 @@ function getPoolStats () {
 
 setInterval(() => {
   try {
-    logger.debug('SQL pool stats', getPoolStats())
+    logger.log('SQL pool stats', getPoolStats())
   } catch (err) {
-    logger.debug('Could not get SQL pool stats', err)
+    logger.log('Could not get SQL pool stats', err)
   }
 }, 60 * 1000)
 
 module.exports = knex
-module.exports.healthcheck = healthcheck
