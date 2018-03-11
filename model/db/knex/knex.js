f130002732a91dc4abb1c055506c07e138bb77bc
nazgul <tamas.kadlecsik@gmail.com>
2018-03-11 15:00:50 +0100

    remove winston for now

--- model/db/knex/knex.js
+++ model/db/knex/knex.js
@@ -4,18 +4,16 @@ const _ = require('lodash')
 const createKnex = require('knex')
 const config = require('../../../config/postgres')
 
-const logger = console
-
 // TLS provided
 if (_.get(config, 'postgres.driver.connection.ssl.ca')) {
-  logger.log('Postgres TLS is enabled')
+  console.log('Postgres TLS is enabled')
 } else {
   delete config.postgres.driver.connection.ssl
 }
 
 const knex = createKnex(config.postgres.driver)
 
-function getPoolStats () {
+function getPoolStats() {
   const pool = knex.client.pool
 
   return {
@@ -30,9 +28,9 @@ function getPoolStats () {
 
 setInterval(() => {
   try {
-    logger.log('SQL pool stats', getPoolStats())
+    console.log('SQL pool stats', getPoolStats())
   } catch (err) {
-    logger.log('Could not get SQL pool stats', err)
+    console.log('Could not get SQL pool stats', err)
   }
 }, 60 * 1000)
 
